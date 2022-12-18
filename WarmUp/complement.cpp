6f1c4bcf7dce22e3743b804965f8b673181406f7
Shohrab Rustam <mohdrustam001@gmail.com>
2022-12-18 22:48:16 +0530

    complement

--- WarmUp/complement.cpp
+++ WarmUp/complement.cpp
@@ -1,11 +1,11 @@
 /**
 **********************************************************************************************************************************************************************************************************************************
-* @file:	DigitToBinary.cpp
+* @file:	complement.cpp
 * @author:	rustam
-* @date:	2022年12月18日 22:12:15 Sunday
-* @brief:	digit to binary
+* @date:	2022年12月18日 22:46:33 Sunday
+* @brief:	complement.cpp
 **********************************************************************************************************************************************************************************************************************************
-**/
+**/     
 #include<bits/stdc++.h>
 using namespace std;
  
@@ -13,12 +13,25 @@ int main()
 {
     int n;
     cin >> n;
-    string binaryNumber = "";
-    
-    while(n){
+        string binaryNumber = "";
+            int result =1;
+        if(n==0){
+            cout << result;
+            return 0;
+        }
+        while(n){
             binaryNumber = to_string(n%2)+binaryNumber; 
             n=n/2;
-    }
-    cout << binaryNumber;
+        }
+        for(int i=0;i<binaryNumber.length();i++){
+            if(binaryNumber[i]=='0'){
+               binaryNumber[i] = '1';
+            }else{
+                binaryNumber[i] = '0';
+            }
+        }
+             result =  stoi(binaryNumber, 0, 2);
+             cout << result;
+ 
     return 0;
 }
