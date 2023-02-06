716ccb4f70b6a83146b20eaced3386eae5fdc3a9
Shohrab Rustam <shohrabrustam@Shohrabs-MacBook-Air.local>
2023-02-06 23:21:53 +0530

    printing an array

--- Array.cpp/printArray.cpp
+++ Array.cpp/printArray.cpp
@@ -8,21 +8,21 @@
 **/
 #include<bits/stdc++.h>
 using namespace std;
- 
+ void printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+ }
 int main()
 {
  int n;
  cin >> n;
  int a[n];
  for (int  i = 0; i < n; i++){
-    printf("a[%d] = %d\n", i, a[i]);
+    cout <<"Please enter the "<<i+1<<". element of an array"<<endl;
     cin >> a[i];
  }
+ printArray(a, n);
 
- {
-    /* code */
- }
- 
- 
     return 0;
 }
